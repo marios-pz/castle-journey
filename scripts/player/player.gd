c52009fa9d00a38e2cd81d5e82f9d569cb8cca14
marios-pz <30930688+marios-pz@users.noreply.github.com>
2025-05-19 18:35:15 +0300

    Somewhat working inventory , removed inventory

--- scripts/player/player.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-@onready var inventory = $Inventory
-
-@onready var item: InvItem = preload("res://assets/player/inventory/items/stick.tres")
-
-func _ready() -> void:
-	print("player loaded")
